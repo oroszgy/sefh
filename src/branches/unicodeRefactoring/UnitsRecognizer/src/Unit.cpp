7a86efad04ca23679ee00949cf2ec484e9dd2452
oroszgy <oroszgy@44bcdd14-f826-11dd-8259-6defae454439>
2009-04-02 09:38:06 +0000

    remove windows version
    git-svn-id: http://sefh.googlecode.com/svn/trunk@28 44bcdd14-f826-11dd-8259-6defae454439

--- src/branches/unicodeRefactoring/UnitsRecognizer/src/Unit.cpp
+++
@@ -1,6 +0,0 @@
-#include "..\include\Unit.h"
-
-Unit::Unit(std::string type_, std::string constant_, double multiplier_):
-type(type_), constant(constant_), multiplier(multiplier_)
-{
-}
--- src/indri/hunTools/include/MorphToolsFactory.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../include/MorphTools.h"
-
-class MorphToolsFactory
-{
-public:
-	static MorphTools* getDefault();
-};
