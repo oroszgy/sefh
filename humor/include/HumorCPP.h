53394b3646c7c076ba1711cc0059bdec0a62c6c2
oroszgy <oroszgy@44bcdd14-f826-11dd-8259-6defae454439>
2009-12-29 11:34:55 +0000

    git-svn-id: http://sefh.googlecode.com/svn/trunk@66 44bcdd14-f826-11dd-8259-6defae454439

--- humor/include/HumorCPP.h
+++ humor/include/HumorCPP.h
@@ -1,12 +1,14 @@
-#ifndef HUMORCPP
-#define HUMORCPP
+#pragma once
 
-#include <gcj/cni.h>
-#include <java/lang/System.h>
-#include <java/lang/Throwable.h>
-#include "../misc/com/weblib/linguist/Humor.h"
+//#include <gcj/cni.h>
+//#include <java/lang/System.h>
+//#include <java/lang/Throwable.h>
+//#include "../misc/com/weblib/linguist/Humor.h"
 #include <string>
 #include <vector>
+#include <iconv.h>
+#include "../include/stem2005_export.h"
+#include "../include/stem2005_options.h"
 
 namespace com
 {
@@ -17,27 +19,34 @@ namespace com
     class HumorCPP
     {
             public:
-                    static void initialize(std::string directory, int langCode);
+                    static void initialize(std::string directory);
                     static bool isInitialized();
                     static int close();
-                    static std::vector<std::string>* getStem(std::string word);
-                    static std::vector<std::string>* getAnalyze(std::string word);
-                    static std::vector<std::string>* getWordGenerate(std::string word);
-                    static std::vector<std::string>* getWordForms(std::string word, int limit);
-                    static std::vector<std::string>* getSyns(std::string word, int contextSize);
-                    void TestError(std::string word, int limit);
+                    static std::vector<std::string> getStem(std::string word);
+                    //static std::vector<std::string>* getAnalyze(std::string word);
+                    //static std::vector<std::string>* getWordGenerate(std::string word);
+                    //static std::vector<std::string>* getWordForms(std::string word, int limit);
+                    static std::vector<std::string> getSyns(std::string word);
+                    //void TestError(std::string word, int limit);
+                    //static std::string* getStrFromJava(java::lang::String* jStr);
+                    //static java::lang::String* convertToJavaStr(std::string* str);
             private:
-                    static std::string* getStrFromJava(java::lang::String* jStr);
-                    static java::lang::String* convertToJavaStr(std::string* str);
-                    static std::vector<std::string>* javaStrArraytoVector(JArray<java::lang::String*>* array);
+                    //static std::vector<std::string>* javaStrArraytoVector(JArray<java::lang::String*>* array);
                     HumorCPP();
+                    static iconv_t utc;
+                    static iconv_t ctu;
+                    static int morphId;
+                    static int stemOptions;
+                    static int synOptions;
+                    static std::vector<std::string> split(std::string str);
 
+                    static iconv_t openCpToUtf();
+                    static void closeDesc (iconv_t conv_desc);
+                    static std::string convert (iconv_t desc, std::string fromStr);
+                    static iconv_t openUtfToCp ();
 
     };
+
     }
   }
 }
-
-
-#endif /*HUMORCPP*/
-
