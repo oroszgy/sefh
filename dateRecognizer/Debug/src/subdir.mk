d7641fef4d99a993e06c83911828af86f0af1fa2
oroszgy <oroszgy@44bcdd14-f826-11dd-8259-6defae454439>
2010-03-22 12:34:59 +0000

    v0.5
    git-svn-id: http://sefh.googlecode.com/svn/tags/0.5@101 44bcdd14-f826-11dd-8259-6defae454439

---
+++ dateRecognizer/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/DateFormat.cpp \
+../src/HunDatefieldAnnotator.cpp 
+
+OBJS += \
+./src/DateFormat.o \
+./src/HunDatefieldAnnotator.o 
+
+CPP_DEPS += \
+./src/DateFormat.d \
+./src/HunDatefieldAnnotator.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"/home/mee/workspaces/sefh/utils/include" -I"/home/mee/workspaces/sefh/indri/contrib/lemur/include" -I"/home/mee/workspaces/sefh/indri/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ dateRecognizer/src/HunDatefieldAnnotator.cpp
@@ -0,0 +1,86 @@
+#include "../include/HunDatefieldAnnotator.h"
+#include "../include/DateFormat.h"
+#include <iostream>
+#include "lemur/lemur-platform.h"
+
+using namespace com::sefh::daterecognition;
+
+HunDatefieldAnnotator::HunDatefieldAnnotator(std::string field) :
+_field(field)  {
+	_dateFormats = NULL;
+	_dateFormats = DateFormat::getInstances();
+}
+
+HunDatefieldAnnotator::~HunDatefieldAnnotator(void)
+{
+	if(_dateFormats != NULL)
+		delete _dateFormats;
+}
+
+void HunDatefieldAnnotator::setHandler(indri::parse::ObjectHandler<indri::api::ParsedDocument> &handler)
+{
+	_handler = &handler;
+}
+
+void HunDatefieldAnnotator::handle(indri::api::ParsedDocument *document)
+{
+	_handler->handle( transform ( document ) );
+}
+
+indri::api::ParsedDocument*  HunDatefieldAnnotator::transform(indri::api::ParsedDocument *document)
+{
+	for( size_t i=0; i<document->tags.size(); i++ ) {
+		indri::parse::TagExtent * extent = document->tags[i];
+          // reparse from the document text
+          if( _field == extent->name ) {
+            int dateStart = document->positions[extent->begin].begin;
+            int dateEnd = document->positions[extent->end-1].end;
+
+			std::string text(document->text);
+			int dateLen = findExtentEnd(dateEnd, text) - dateStart ;
+            std::string date;
+
+			date = text.substr(dateStart, dateLen);
+
+			//DEbug
+			//std::cout<<date;
+
+            _parseDate(date, extent);
+          }
+        }
+        return document;
+}
+
+int HunDatefieldAnnotator::findExtentEnd(int supposedEnd, std::string text)
+{
+	while(text[supposedEnd] != '<')
+		++supposedEnd;
+	return supposedEnd;
+}
+
+//P: Exists a parser which can recognize
+void HunDatefieldAnnotator::_parseDate(const std::string& _date, indri::parse::TagExtent *extent) const
+{
+	std::string day, month, year;
+
+	DateFormat* matchingDateFormat = NULL;
+	for(size_t i=0; i<_dateFormats->size(); ++i)
+	{
+		if(boost::regex_match(_date, _dateFormats->at(i).getRecognizerRegExp()))
+		{
+			matchingDateFormat = &(_dateFormats->at(i));
+			break;
+		}
+	}
+
+	//TODO: remove it in the release
+	// if there is no mathcing date format skip
+	if(matchingDateFormat == NULL)
+		return;
+
+	month = matchingDateFormat->getMonthIntegerString(_date);
+	year = matchingDateFormat->getYear(_date);
+	day = matchingDateFormat->getDay(_date);
+
+	extent->number = indri::parse::DateParse::convertDate(year, month, day);
+}
