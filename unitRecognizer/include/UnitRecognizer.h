a0452f04843d7dacb7d24e515ed9309b279071c0
oroszgy <oroszgy@44bcdd14-f826-11dd-8259-6defae454439>
2010-03-22 12:12:22 +0000

    git-svn-id: http://sefh.googlecode.com/svn/tags/0.5@90 44bcdd14-f826-11dd-8259-6defae454439

---
+++ unitRecognizer/include/UnitRecognizer.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <vector>
+#include <map>
+#include <string>
+#include "../../utils/include/Unit.h"
+
+namespace com
+{
+namespace sefh
+{
+namespace unitrecognition
+{
+/**
+ * Utility class which stores number formats, and can generate recognizing regular
+ * expressions.
+ */
+class UnitRecognizer
+{
+public:
+	~UnitRecognizer();
+
+	/**
+	 * Returns the default instance.
+	 */
+	static UnitRecognizer* getDefault();
+	/**
+	 * Returns a regular expression which can recognize the given unit.
+	 */
+	std::string getRecognizingRegExpFromConstants(std::string);
+	/**
+	 * Returns a regular expression which can recognize units.
+	 */
+	std::string getRecognizingRegExp();
+	/**
+	 * Returns the SI value of the stored unit.
+	 */
+	double getSIValue(std::string);
+	/**
+	 * Returns the type of the unit.
+	 */
+	std::string getTypeOfMeasure(std::string);
+	/**
+	 * Returns the unit recognition regular expressions.
+	 */
+	std::map<std::string, std::string>* getUnitsRecognizingRegexps();
+
+private:
+	UnitRecognizer();
+	static UnitRecognizer* defaultInstance;
+	std::vector<Unit>* units;
+	std::vector<std::string>* separators;
+	std::string decSep;
+};
+}
+}
+}
+
