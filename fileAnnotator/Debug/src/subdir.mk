1ac50cef27e2761a294fb61849bb80c8a2102f3e
oroszgy <oroszgy@44bcdd14-f826-11dd-8259-6defae454439>
2010-03-22 12:12:01 +0000

    git-svn-id: http://sefh.googlecode.com/svn/tags/0.5@88 44bcdd14-f826-11dd-8259-6defae454439

---
+++ fileAnnotator/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/FieldAnnotator.cpp \
+../src/Main.cpp 
+
+OBJS += \
+./src/FieldAnnotator.o \
+./src/Main.o 
+
+CPP_DEPS += \
+./src/FieldAnnotator.d \
+./src/Main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"/home/mee/workspaces/sefh/dateRecognizer/include" -I"/home/mee/workspaces/sefh/utils/include" -I"/home/mee/workspaces/sefh/unitRecognizer/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
