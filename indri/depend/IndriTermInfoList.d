a3dcbbbc2d8b9acdaf0d92847d365e5881e775ea
oroszgy <oroszgy@44bcdd14-f826-11dd-8259-6defae454439>
2010-03-22 12:34:35 +0000

    v0.5
    git-svn-id: http://sefh.googlecode.com/svn/tags/0.5@98 44bcdd14-f826-11dd-8259-6defae454439

---
+++ indri/depend/IndriTermInfoList.d
@@ -0,0 +1,17 @@
+IndriTermInfoList.o ../depend/IndriTermInfoList.d : ../src/IndriTermInfoList.cpp \
+  ../include/indri/TermList.hpp ../include/indri/greedy_vector \
+  ../contrib/lemur/include/lemur/lemur-platform.h \
+  ../contrib/lemur/include/lemur/lemur-compat.hpp \
+  ../contrib/lemur/include/lemur/lemur-platform.h \
+  ../include/indri/FieldExtent.hpp \
+  ../contrib/lemur/include/lemur/RVLCompress.hpp \
+  ../include/indri/Buffer.hpp ../include/indri/RVLCompressStream.hpp \
+  ../include/indri/RVLDecompressStream.hpp \
+  ../contrib/lemur/include/lemur/IndexTypes.hpp \
+  ../include/indri/IndriTermInfoList.hpp \
+  ../contrib/lemur/include/lemur/InvFPTypes.hpp \
+  ../contrib/lemur/include/lemur/common_headers.hpp \
+  ../contrib/lemur/include/lemur/IndexTypes.hpp \
+  ../contrib/lemur/include/lemur/TermInfoList.hpp \
+  ../contrib/lemur/include/lemur/Exception.hpp \
+  ../include/indri/count_iterator ../include/indri/greedy_vector
---
+++ indri/depend/WordDocumentExtractor.d
@@ -0,0 +1,10 @@
+WordDocumentExtractor.o ../depend/WordDocumentExtractor.d : ../src/WordDocumentExtractor.cpp \
+  ../include/indri/WordDocumentExtractor.hpp \
+  ../contrib/lemur/include/lemur/lemur-compat.hpp \
+  ../contrib/lemur/include/lemur/lemur-platform.h \
+  ../include/indri/Buffer.hpp ../include/indri/UnparsedDocument.hpp \
+  ../include/indri/MetadataPair.hpp ../include/indri/greedy_vector \
+  ../contrib/lemur/include/lemur/lemur-platform.h \
+  ../contrib/lemur/include/lemur/Exception.hpp \
+  ../include/indri/DocumentIterator.hpp \
+  ../include/indri/ObjectHandler.hpp
