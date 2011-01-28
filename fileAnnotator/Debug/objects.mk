9e8e51bbd470c979988fcb9920d9891206595d9b
oroszgy <oroszgy@gmail.com>
2011-01-28 10:09:35 +0100

    Quickfix for general running.

--- fileAnnotator/Debug/objects.mk
+++ fileAnnotator/Debug/objects.mk
@@ -2,6 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := /home/mee/workspaces/sefh/sefh/utils/Debug/libutils.a
+USER_OBJS := /home/oroszgy/workspace/ppke/hundri/utils/Debug/libutils.a
 
 LIBS := -lboost_regex-mt -lunitRecognizer -ldateRecognizer
+
