e9fb6da6637b735c027b290d592b9887af570e42
oroszgy <oroszgy@gmail.com>
2011-01-28 10:10:11 +0100

    Quickfix

--- unitRecognizer/Debug/src/subdir.mk
+++ unitRecognizer/Debug/src/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/home/mee/workspaces/sefh/sefh/utils/include" -I"/home/mee/workspaces/sefh/sefh/indri/contrib/lemur/include" -I"/home/mee/workspaces/sefh/sefh/indri/include" -I"/home/mee/workspaces/sefh/sefh/utils/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I"/home/oroszgy/workspace/ppke/hundri/utils/include" -I"/home/oroszgy/workspace/ppke/hundri/indri/contrib/lemur/include" -I"/home/oroszgy/workspace/ppke/hundri/indri/include" -I"/home/oroszgy/workspace/ppke/hundri/utils/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
